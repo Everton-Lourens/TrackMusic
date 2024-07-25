59a8700ca495fd0a0bf3373a7d7dafb4ad8c3c86
Everton-Lourens <123413870+Everton-Lourens@users.noreply.github.com>
2024-07-25 16:01:47 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,22 +1,8 @@
 /**
  * @format
  */
-/*
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
-*/
-
 import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-import TrackPlayer from 'react-native-track-player';
-// @ts-ignore
-import service from '@/service';
-
-// Registre o serviço
-TrackPlayer.registerPlaybackService(() => service);
+import App from '@/App';
+import { name as appName } from '@/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
